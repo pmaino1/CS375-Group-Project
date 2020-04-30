9f7c279760cd861bd74cfc39d94a581d5e2ec467
Matthew Kyea <mkyea1@binghamton.edu>
2020-04-30 17:45:13 -0400

    cleaned a little

--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@ class Node {
 		int data;
 		char color; //g,w,b
 		vector<Node*> adj;
-		//maybe include a parent node, maybe include
-		//vector of nodes that point here
 	public:
 		static int ids;
 		Node(int d) {
@@ -24,7 +22,6 @@ class Node {
 			data = d;
 			color = 'w';		
 		}
-		//~Node();
 		int getData() { return data; }
 		char getColor() { return color; }
 		int getId() { return id; }
@@ -36,10 +33,10 @@ class Node {
 			cout<<"NODE "<< id << ", " << "data: "<< data <<",  color: "<< color <<endl;
 			cout << "---ADJ TO: ";
 			if(!adj.empty()){ 
-        for (auto it = adj.begin(); it != adj.end(); it++) {
+        			for (auto it = adj.begin(); it != adj.end(); it++) {
 				  cout << (*it)->getId() << "("<< (*it)->getData() << "," << (*it)->getColor() << "), ";
 			  }
-      }
+      			}
 			cout << endl;
 		}
 };
@@ -49,10 +46,8 @@ class Graph {
 	private:
 		vector<Node*> graph;
 	public:
-		//Graph();
 		void addNode(Node * n) { graph.push_back(n); }
 		vector<Node*>  getGraph() { return graph; }
-
 		void print() {
 			for (auto it = graph.begin(); it != graph.end(); it++) {
 				(*it)->print();
@@ -62,7 +57,7 @@ class Graph {
 
 void DFSVisit(Graph g, Node * currNode){
   currNode->setColor('g');
-  //currNode->print();
+  currNode->print();
   vector<Node *> adjList = currNode->getAdj();
   for(auto it = adjList.begin(); it != adjList.end(); it++){
     if( (*it)->getColor() == 'w' ) DFSVisit(g, *it); 
@@ -79,9 +74,6 @@ void DFS(Graph g){
 
 
 int Node::ids = 0;
-
-
-//next time - generator for increasing density, size, fix one and increase the other, etc
 
 //give #numNodes, every node has numNodes/2 adjacents
 Graph * generateGraphsSize(int numNodes){
@@ -208,31 +200,11 @@ void tester(){
 }
 
 int main(int argc, char** args) {
-	
-/*
-	Node n(2);
-	Node m(4);
-	Node l(1);
-  Node z(7);
 
-	n.addAdj(&m);
-	m.addAdj(&l);
-	l.addAdj(&n);
-  n.addAdj(&z);
-  n.addAdj(&l);
-
-	Graph g;
-	g.addNode(&n);
-	g.addNode(&m);
-	g.addNode(&l);
-  g.addNode(&z);
-
-	//g.print();
-  */
-  //cout << (&n)->getColor() <<endl;
-
-/*
-
+  //creation of nodes
+  //parameter provides data, 
+  //constructor sets color to white
+  //and assigns unique ID
   Node one(1);
   Node two(2);
   Node three(3);
@@ -241,17 +213,18 @@ int main(int argc, char** args) {
   Node six(6);
   Node seven(7);
   Node eight(8);
-  Node nine(9);
   
+  //setting up the edges
+  //order matters
   one.addAdj(&two);
-  one.addAdj(&three);
-  two.addAdj(&four);
-  two.addAdj(&five);
-  three.addAdj(&six);
-  three.addAdj(&seven);
-  four.addAdj(&eight);
-  eight.addAdj(&nine);
+  one.addAdj(&four);
+  one.addAdj(&five);
+  two.addAdj(&three);
+  five.addAdj(&six);
+  six.addAdj(&seven);
   
+  //adding nodes to graph
+  //order matters
   Graph h;
   h.addNode(&one);
   h.addNode(&two);
@@ -261,14 +234,17 @@ int main(int argc, char** args) {
   h.addNode(&six);
   h.addNode(&seven);
   h.addNode(&eight);
-  h.addNode(&nine);
   
+
+  cout<<"\nBefore DFS"<<endl;
+  h.print();
+  cout<<"\nDuring DFS"<<endl;
   DFS(h);
+  cout<<"\nAfter DFS"<<endl;
+  h.print();
 
 
-*/
+  //tester();
 
-  tester();
-
-	return 0;
+  return 0;
 }
